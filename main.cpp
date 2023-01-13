6244c68c4a0488eef727967d702110c972f2ebae
s-alad <n7.saad@gmail.com>
2023-01-12 22:34:57 -0500

    ini

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 
 int main() {
-
+    std::cout << "MAIN" << std::endl;
     return 0;
 }
+
+class Block {
+    public:
+        std::string hash;
+        std::string previousHash;
+        Block (int index, const std::string &data);
+        std::string calculateHash() const;
+        void mineBlock(int difficulty);
+    private:
+        int index;
+        long timeStamp;
+        std::string data;
+        int nonce;
+};
